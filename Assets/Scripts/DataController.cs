790291149ffa0123a25b33a9b449a08d46941642
Felicia <agatha.flc@hotmail.com>
2018-02-04 14:50:20 +0800

    ADD: reading json implementation

--- Assets/Scripts/DataController.cs
+++ Assets/Scripts/DataController.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using System.IO;
 
 public class DataController : MonoBehaviour {
 
-	public RoundData[] allRoundData;
+	private RoundData[] allRoundData;
 
 	private PlayerProgress playerProgress;
+	private string gameDataFileName = "data.json";
 
 	// Use this for initialization
 	void Start () {
 		DontDestroyOnLoad (gameObject); // prevent destroy objects in previous scene that has been unloaded
+		LoadGameData();
 		LoadPlayerProgress();
 
 		SceneManager.LoadScene ("MenuScreen");
@@ -43,5 +45,18 @@ public class DataController : MonoBehaviour {
 
 	private void SavePlayerProgress() {
 		PlayerPrefs.SetInt ("highestScore", playerProgress.highestScore);
+	}
+
+	private void LoadGameData(){
+		string filePath = Path.Combine (Application.streamingAssetsPath, gameDataFileName); // streamingAssetsPath is the folder that stores the json
+
+		if (File.Exists (filePath)) {
+			string dataAsJson = File.ReadAllText (filePath);
+			GameData loadedData = JsonUtility.FromJson<GameData> (dataAsJson); // turn from json to an object
+
+			allRoundData = loadedData.allRoundData;
+		} else {
+			Debug.LogError ("Cannot load game data!");
+		}
 	}
 }
