73c1fdce6d55a30b21bd9d9071f18acb02d507fc
Felicia <agatha.flc@hotmail.com>
2018-02-04 18:09:10 +0800

    ADD: sample expressionData json

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using System.Collections.Generic;
 
 public class GameController : MonoBehaviour {
 
