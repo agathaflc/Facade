3ee6f6dab86e682983e30748e768a6899cec017a
Felicia <agatha.flc@hotmail.com>
2018-02-27 15:25:43 +0800

    UPT: made changes to detective responses

---
+++ Assets/Scripts/DetectiveResponses.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DetectiveResponses {
+
+	public ResponseData[] suspiciousPositive;
+	public ResponseData[] suspiciousNegative;
+	public ResponseData[] suspiciousNeutral;
+	public ResponseData[] notSuspiciousPositive;
+	public ResponseData[] notSuspiciousNegative;
+	public ResponseData[] notSuspiciousNeutral;
+}
---
+++ Assets/Scripts/ResponseData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ResponseData {
+
+	public string text;
+	public string soundFilePath;
+}
