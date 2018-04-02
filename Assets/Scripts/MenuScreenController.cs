1dd29b984f8eb18775f7298089b1313864c153cd
Felicia <agatha.flc@hotmail.com>
2018-04-02 15:33:39 +0800

    ADD: room and hans

--- Assets/Scripts/MenuScreenController.cs
+++ Assets/Scripts/MenuScreenController.cs
@@ -5,7 +5,7 @@ public class MenuScreenController : MonoBehaviour
 {
     public void StartGame()
     {
-		Initiate.Fade ("Game", Color.black, 0.8f);
+		Initiate.Fade ("GameScene", Color.black, 0.8f);
         //SceneManager.LoadScene("Game");
     }
 
