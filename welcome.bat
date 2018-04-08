b6749f676ef984b47c6f3930912b1ceba436aed6
Felicia <agatha.flc@hotmail.com>
2018-04-08 20:37:07 +0800

    UPT: game title, ADD: batch file but still need to change paths later

---
+++ welcome.bat
@@ -0,0 +1,11 @@
+@echo off
+title FACADE LAUNCHER
+echo **** YOU SHOULD ALREADY HAVE THE ANACONDA ENVIRONMENT ALREADY SET UP AT THIS POINT ****
+pause
+set @myvar="C:\Users\agath\Anaconda3\Scripts\"
+echo %@myvar%
+start Facade.exe
+call %@myvar%\activate.bat cv2
+cd ".\Assets\FERModel\"
+call python video_emotion_color_demo.py
+pause
