444f2abcfd5d41839ee4f28b71b33daf58d4ed7f
Felicia <agatha.flc@hotmail.com>
2018-04-23 18:22:06 +0800

    ADD: gradual special effects

--- Assets/Scripts/SpecialEffect.cs
+++ Assets/Scripts/SpecialEffect.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class SpecialEffect
 {
     public string type; // "vignette", "bloom", or "motionBlur"
-    public float intensity; // for bloom and vignette
+    
+    public string status; // only for motion blur cos everything else can be indicated by intensity
+    
+    public float intensity; // for bloom (0~infinity (i'd say max 8)), vignette (0~1)
 
-    // specifically for vignette
+    // for motion blur
+    public float shutterAngle; // 0~360
+    
+    // for vignette
     public Color color;
     public float colorR;
     public float colorG;
     public float colorB;
     public float colorA;
-    public float smoothness;
-    public float roundness;
+    public float smoothness; // 0~1
+    public float roundness; // 0~1
 }
