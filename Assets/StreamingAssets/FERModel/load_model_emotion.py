7218b36c4f185161aa26ab5ecd4f03aca9f693cc
Vivian Rajkumar <vivian.rajkumar@hotmail.com>
2018-05-03 00:54:19 +0800

    Cleaned up code

--- Assets/StreamingAssets/FERModel/load_model_emotion.py
+++ Assets/StreamingAssets/FERModel/load_model_emotion.py
@@ -1,17 +1,13 @@
+# imports
 import sys
 import args
-
 req_link = '/usr/local/lib/python2.7/site-packages/'
 sys.path.append(req_link)
-
 import os
-
 from keras import backend as K
 K.set_image_dim_ordering('th')
-
 import numpy as np
 np.random.seed(123)  # for reproducibility
-
 from keras import metrics
 from keras.callbacks import EarlyStopping, TensorBoard
 from keras.models import Sequential
@@ -31,23 +27,23 @@ def load_model(path_inputs="saved_models/multiclass_models/model.json", path_wei
     loadmodel = json_file.read()
     json_file.close()
     model = model_from_json(loadmodel)
-
     # load weights into new model
     model.load_weights(path_weights)
     print("Loaded model from disk")
-
     # compile model
     model.compile(loss='categorical_crossentropy',
                   optimizer='adam',
                   metrics=['accuracy'])
-
     print("Compiled loaded model")
+    
     return model
 
 
 def eval_model(model, X_test, Y_test):
+    
     score = model.evaluate(X_test, Y_test, verbose=0)
     # print(X_test, Y_test)
     print('Test loss:', score[0])
     print('Test accuracy:', score[1])
+    
     return score
